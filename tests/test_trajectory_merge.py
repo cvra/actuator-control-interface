3fe837331c646387eb91cfc3f1dd95b56704807b
Antoine Albertelli <antoinea101@gmail.com>
2015-05-13 17:34:45 +0200

    Add docstrings to tests

--- tests/test_trajectory_merge.py
+++ tests/test_trajectory_merge.py
@@ -4,6 +4,10 @@ from trajectory_publisher import *
 
 class TrajectoryMergingTestCase(unittest.TestCase):
     def test_can_merge_trajectory(self):
+        """
+        Simple check that we can merge a second trajectory overlapping with the
+        first (most common case.
+        """
         dt = 0.1
         first = Trajectory(1., dt, (1, 2, 3))
         second = Trajectory(1. + dt, dt, (10, 20, 30))
@@ -12,6 +16,10 @@ class TrajectoryMergingTestCase(unittest.TestCase):
         self.assertEqual(res, Trajectory(1., dt, (1, 10, 20, 30)))
 
     def test_can_merge_trajectory_before(self):
+        """
+        This test checks that having a second trajectory starting before the
+        start of the first one works as expected.
+        """
         dt = 0.1
         first = Trajectory(1., dt, (1, 2, 3))
         second = Trajectory(1. - dt, dt, (10, 20, 30))
