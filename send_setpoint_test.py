784122ae94e9074577d79dbc8afc69391434545d
Antoine Albertelli <antoinea101@gmail.com>
2015-05-17 02:29:20 +0200

    Add a script to test sending setpoints

---
+++ send_setpoint_test.py
@@ -0,0 +1,40 @@
+from cvra_rpc.service_call import call
+import argparse
+from trajectory_publisher import *
+import time
+
+
+HOST = '192.168.3.20'
+
+
+def create_actuator(name):
+    return call((HOST, 20001), 'actuator_create_driver', [name])
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('host')
+parser.add_argument('value', type=float)
+parser.add_argument('--actuator', '-a',  default='foobar2000',
+                    help="Actuator to set (default foobar2000)")
+
+parser.add_argument('--create', '-c', action='store_true')
+parser.add_argument('--position', '-p', action='store_true')
+parser.add_argument('--speed', '-s', action='store_true')
+
+args = parser.parse_args()
+
+if args.create:
+    print(create_actuator(args.actuator))
+
+pub = SimpleRPCActuatorPublisher((args.host, 20000))
+
+if args.position:
+    pub.update_actuator(args.actuator, PositionSetpoint(args.value))
+elif args.speed:
+    pub.update_actuator(args.actuator, SpeedSetpoint(args.value))
+
+print(pub.get_state(args.actuator, time.time()))
+
+pub.publish(time.time())
+
